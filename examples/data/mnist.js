c58e470736b3c08cd7607b4c4ce3014b7f519adc
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2016-08-29 16:42:23 -0700

    Port VAE example into test infrastructure.

--- examples/data/mnist.js
+++ examples/data/mnist.js
@@ -5,7 +5,7 @@ var path = require('path');
 var assert = require('assert');
 var _ = require('underscore');
 
-// Download and gunzip training set from:
+// Download and gunzip training and test sets from:
 // http://yann.lecun.com/exdb/mnist/
 
 // JSON output written to current directory.
@@ -19,19 +19,21 @@ var baseDir = process.argv[2];
 
 var trainImagesFilename = 'train-images-idx3-ubyte';
 var trainLabelsFilename = 'train-labels-idx1-ubyte';
+var testImagesFilename = 't10k-images-idx3-ubyte';
+var testLabelsFilename = 't10k-labels-idx1-ubyte';
 
-var loadImages = function(fn) {
+var loadImages = function(fn, nImages) {
   var buff = fs.readFileSync(fn);
 
   assert.strictEqual(buff.readUInt32BE(0), 2051);
-  assert.strictEqual(buff.readUInt32BE(4), 60000);
+  assert.strictEqual(buff.readUInt32BE(4), nImages);
   assert.strictEqual(buff.readUInt32BE(8), 28);
   assert.strictEqual(buff.readUInt32BE(12), 28);
 
   var offset = 16;
   var images = [];
 
-  for (var i = 0; i < 60000; i++) {
+  for (var i = 0; i < nImages; i++) {
     var image = [];
     for (var j = 0; j < 784; j++) {
       var pixelIntensity = buff[offset + (i * 784) + j];
@@ -44,16 +46,16 @@ var loadImages = function(fn) {
   return images;
 };
 
-var loadLabels = function(fn) {
+var loadLabels = function(fn, nLabels) {
   var buff = fs.readFileSync(fn);
 
   assert.strictEqual(buff.readUInt32BE(0), 2049);
-  assert.strictEqual(buff.readUInt32BE(4), 60000);
+  assert.strictEqual(buff.readUInt32BE(4), nLabels);
 
   var offset = 8;
   var labels = [];
 
-  for (var i = 0; i < 60000; i++) {
+  for (var i = 0; i < nLabels; i++) {
     labels.push(buff[offset + i]);
   }
 
@@ -69,8 +71,14 @@ var showImage = function(image) {
   }
 };
 
-var images = loadImages(path.join(baseDir, trainImagesFilename));
-fs.writeFileSync('mnist_images.json', JSON.stringify(images));
+// Train
+var images_train = loadImages(path.join(baseDir, trainImagesFilename), 60000);
+fs.writeFileSync('mnist_images_train.json', JSON.stringify(images_train));
+var labels_train = loadLabels(path.join(baseDir, trainLabelsFilename), 60000);
+fs.writeFileSync('mnist_labels_train.json', JSON.stringify(labels_train));
 
-var labels = loadLabels(path.join(baseDir, trainLabelsFilename));
-fs.writeFileSync('mnist_labels.json', JSON.stringify(labels));
+// Test
+var images_test = loadImages(path.join(baseDir, testImagesFilename), 10000);
+fs.writeFileSync('mnist_images_test.json', JSON.stringify(images_test));
+var labels_test = loadLabels(path.join(baseDir, testLabelsFilename), 10000);
+fs.writeFileSync('mnist_labels_test.json', JSON.stringify(labels_test));
