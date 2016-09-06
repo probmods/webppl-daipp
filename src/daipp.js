ad7ffe1382320a052a59af429ed16c794a19bc97
null-a <null-a@users.noreply.github.com>
2016-09-06 11:23:37 +0100

    Remove stale comment.
    There's no longer any fall through here.

--- src/daipp.js
+++ src/daipp.js
@@ -153,12 +153,6 @@ module.exports = function(env) {
       }
       //otherwise treat as enum: only equal objects have same vec.
       return nneval(getConstant(val));
-
-      // FIXME: The fall through here means the all objects not
-      // implementing embed2vec are treat as null. Fix by checking
-      // cache handles null and removing the case below? I'd also be
-      // tempted to not rely on fall through at all, it's a frequent
-      // source of bugs.
     case "null":
       val = "iamnull" //just in case cache doesn't deal properly with null key.
     default:
