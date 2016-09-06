7ce0427ec5c1edb3eb47290fc1718a620109c30b
null-a <null-a@users.noreply.github.com>
2016-09-06 14:47:09 +0100

    Clean-up nneval commentary.

--- src/nneval.js
+++ src/nneval.js
@@ -37,36 +37,17 @@ module.exports = function(env, config) {
     };
   }
 
-  // dritchie: We need a function that wraps any call to nn.eval(), which will do parameter registration
-  // -------------
-  // Doing this in raw WebPPL would be incorrect; the address at each call to eval() could be different,
-  //    and so we'd end up registering multiple sets of parameters for the same network
-  // Doing parameter registration on nn creation would allow us to use the current address, but presents
-  //    other problems: (1) some nets are created at the global scope, outside any inference thunk (see
-  //    the nets in DAIPP.wppl); (2) other nets are memoized, so parameter registration will not happen
-  //    if multiple coroutines are called (e.g. in EUBO followed by SMC, params passed into SMC will not
-  //    be registered to the nn's because their cached creation functions won't be called again).
-  // -------------
+  // By using this function throughout daipp (rather than calling
+  // nn.eval directly) we ensure that inference coroutines are aware
+  // of the parameters of all neural network.
   function nneval(nn, arg) {
-    // TODO: parameter registration (only if the nn has > 0 parameters)
-    // We will need a non-CPS'ed 'registerParams' that takes an explicit name/address
-    // This also needs to incorporate the base address of the current coroutine, so that the parameter
-    //    relative addressing scheme works, and also so nested inference works with DAIPP.
-
-    // registerParams is made globally available in the WebPPL header.
     if (nn.getParameters().length > 0) {
       assert.ok(nn.name && nn.name.length > 0, 'daipp: Parameterized net cannot be anonymous.');
       util.registerParams(env, nn.name,
                           config.useXavierInit ? wrapGetParamsWithXavier(nn) : nn.getParameters.bind(nn),
                           nn.setParameters.bind(nn));
     }
-
-    // Fast version, assuming all nets take at most one argument
     return nn.eval(arg);
-
-    // Less efficient, fully-general version using varargs
-    // var NN = Object.getPrototype(nn);
-    // return NN.eval.apply(nn, Array.prototype.slice.call(arguments, 1));
   }
 
   return nneval;
